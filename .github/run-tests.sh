aaae122d3340b3c500dec33ea8131d53ce9ea19c
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2023-01-11 17:13:54 +0100

    ci: add shellcheck

---
+++ .github/run-tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+# https://github.com/dylanaraps/pure-bash-bible
+
+shellcheck *.sh
